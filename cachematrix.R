8421f18e8d500ee2864dfc95517320bf7852713b
deedjunk@gmail.com <yyong@corp.logitech.com>
2016-01-24 17:11:23 -0800

    for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment to store and retrieve matrixes and its corresponding inverse matrixes
+## the values of the inverse is cached should it be called more than once
 
-## Write a short comment describing this function
+
+## creating a matrix object that stores the content of said matrix, and also the inverse of it
+## should it be called more than once
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		iM <- NULL
+        set <- function(y) {
+				x <<- y
+                im <<- NULL
+        }
+        get <- function() x
+        setinvMatrix <- function(IM) 
+		{
+			iM <<- IM
+		}	
+        getinvMatrix <- function() 
+		{
+			iM
+		}
+        list(set = set, get = get,
+             setinvMatrix = setinvMatrix,
+             getinvMatrix = getinvMatrix)
 }
 
 
-## Write a short comment describing this function
+## if the inverse is called once, it will be computed
+## if its called more than once, it retrieves the cached value and get returned
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		IM <- x$getinvMatrix()
+        if(!is.null(IM)) {
+                message("getting cached data")
+                return(IM)
+        }
+        data <- x$get()		
+        IM <- solve(data, ...)
+        x$setinvMatrix(IM)
+        IM
 }
